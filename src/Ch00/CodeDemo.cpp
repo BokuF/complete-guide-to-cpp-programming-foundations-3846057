e8ed54ba9a12752107c7681e4c793f98c9442e95
BokuF <add11ss@yahoo.com>
2025-01-17 02:13:43 +0000

    First Ex_001

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -2,20 +2,15 @@
 // Exercise 00_03
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 //bokus first changes
+
 #include <iostream>
+#include <string>
 
 int main(){
-    float num_1, num_2, result;
+    
+std::cout << "           Melanie Dental Clinic" << std::endl;
+    std::cin >>
 
-    std::cout << "Enter number 1: " << std::flush;
-    std::cin >> num_1;
-    std::cout << "Enter number 2: " << std::flush;
-    std::cin >> num_2;
-    
-    result = num_1 + num_2;
-    
-    std::cout << "The result of the addition is " << result << std::endl;
-    
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,3 +2,15 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 // Hello World, by Mr Boku the best
+
+#include <iostream>
+#include <string>
+
+int main(){
+
+    std::cout << "           Melanie Dental Clinic" << std::endl;
+    std::cin >>
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
